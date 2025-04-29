88484334a10b995a54864373d356d929fabe328a
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-04-29 18:56:12 -0400

    added do while logic to loop rectangle processing

--- Chapter 6 Programming Projects/Chapter 6 Programming Projects.cpp
+++ Chapter 6 Programming Projects/Chapter 6 Programming Projects.cpp
@@ -10,28 +10,42 @@ int main()
 {
 	float RectangleLength;
 	float RectangleWidth;
+	float RectangleArea;
+	float RectanglePerimeter;
+	int UserChoice;
 
+	do {
 
-   cout << "Input Rectangle Length:\n";
-   cin >> RectangleLength;
+		//Request User inputs for length and width
+		cout << "Input Rectangle Length:\n";
+		cin >> RectangleLength;
 
-   cout << "Input Rectangle Width:\n";
-   cin >> RectangleWidth; 
+		cout << "Input Rectangle Width:\n";
+		cin >> RectangleWidth;
 
-   if (
+		//Input validation
+		if (RectangleLength < 0 || RectangleWidth < 0)
+		{
+			cout << "Input Invalid. Values must be greater than zero.\n";
+		}
 
-	   RectangleLength || RectangleWidth < 0; 
+		//Area and Perimeter Calculation
+		else
+		{
+			RectangleArea = RectangleLength * RectangleWidth;
+			cout << "The area of the rectangle is: " << RectangleArea << endl;
+			RectanglePerimeter = (2 * RectangleLength) + (2 * RectangleWidth);
+			cout << "The perimeter of the rectangle is: " << RectanglePerimeter << endl;
+		}
 
-	 then 
+		//Process another rectangle or quit
+		cout << "Enter 1 to process another rectangle. Enter any other value to quit." << endl;
+
+		cin >> UserChoice;
+
+	}  while (UserChoice == 1);
+
+	cout << "Goodbye." << endl;
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
