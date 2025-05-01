42dd16fc7ded897543b6ed2d6c022f6326e9a90a
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-05-01 11:00:59 -0400

    added get number of rooms function with validation

--- Chapter 6 Programming Projects/Chapter 6 Programming Projects.cpp
+++ Chapter 6 Programming Projects/Chapter 6 Programming Projects.cpp
@@ -1,77 +1,22 @@
 // Chapter 6 Programming Projects.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-// This program calculates final scoring for a performance based on 5 judge raw score inputs 0-10.
+// This program calculates and displays the labor and paint costs of a painting job based on number of rooms, paint, and labor costs.
 
 #include <iostream>
 
 
 using namespace std;
 
-//Function to get 5 judge scores
-void GetRawScores( float & Score) 
-{
-	do {
-		cout << "Enter judge's score (0 - 10):\n";
-		cin >> Score;
-
-		if (Score < 0 || Score > 10)
-		{
-			cout << "Invalid input. Score must be between 0 and 10.\n";
-		}
-	}
-	while (Score < 0 || Score > 10);
-	
-}
-
-//Function to find highest score
-float FindHighestScore(float S1, float S2, float S3, float S4, float S5)
-{
-	float Highest = S1;
-	if (S2 > Highest) Highest = S2;
-	if (S3 > Highest) Highest = S3;
-	if (S4 > Highest) Highest = S4;
-	if (S5 > Highest) Highest = S5;
-	return Highest;
-}
-
-//Fucntion to find lowest score
-float FindLowestScore(float S1, float S2, float S3, float S4, float S5)
-{
-	float Lowest  = S1;
-	if (S2 < Lowest) Lowest = S2; 
-	if (S3 < Lowest) Lowest = S3; 
-	if (S4 < Lowest) Lowest = S4; 
-	if (S5 < Lowest) Lowest = S5; 
-	return Lowest; 
-}
-
-//Function to drop highest and lowest then average middle three scores
-float CalcFinalScore(float S1, float S2, float S3, float S4, float S5)
-{
-	float Lowest = FindLowestScore(S1, S2, S3, S4, S5);
-	float Highest = FindHighestScore(S1, S2, S3, S4, S5);
-	float FinalScore = (S1 + S2 + S3 + S4 + S5) - Lowest - Highest;
-	return FinalScore / 3;
-}
-
-//Function to display score
-void DisplayScore(float AverageScore) 
-{
-	cout << "Final Calculated Average Score: " << AverageScore << endl;
-}
-
-int main()
-{
-	float S1, S2, S3, S4, S5;
-
-	GetRawScores(S1);
-	GetRawScores(S2);
-	GetRawScores(S3);
-	GetRawScores(S4);
-	GetRawScores(S5);
-
-	float AverageScore = CalcFinalScore(S1, S2, S3, S4, S5);
-	DisplayScore(AverageScore);
-
-	return 0;
+//User input and validate number of rooms
+int GetNumberOfRooms() {
+    int Rooms; 
+    do {
+        cout << "Enter the number of rooms to be painted (1 room minimum):\n";
+        cin >> Rooms; 
+        if (Rooms < 1) 
+        {
+            cout << "Invalid input. You must paint at least one room.\n";
+        }
+    } while (Rooms < 1); 
+    return Rooms; 
 }
