bb67a85c74d025460b77b2a6c98693b1690b6124
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-05-01 10:10:24 -0400

    added function to get raw scores with input validation

--- Chapter 6 Programming Projects/Chapter 6 Programming Projects.cpp
+++ Chapter 6 Programming Projects/Chapter 6 Programming Projects.cpp
@@ -7,4 +7,20 @@
 
 using namespace std;
 
+//Function to get 5 judge scores
+void GetRawScores( float & Score) 
+{
+	do {
+		cout << "Enter judge's score (0 - 10):\n";
+		cin >> Score;
+
+		if (Score < 0 || Score > 10)
+		{
+			cout << "Invalid input. Score must be between 0 and 10.\n";
+		}
+		while (Score < 0 || Score > 10);
+	}
+}
+
+
 
