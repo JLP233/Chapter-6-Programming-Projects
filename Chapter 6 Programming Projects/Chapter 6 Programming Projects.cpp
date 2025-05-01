30b819241cbc693c95717bf9160fd31b41c2a0cb
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-04-30 20:35:08 -0400

    completed and tested GetAccidents and FindLowest functions

--- Chapter 6 Programming Projects/Chapter 6 Programming Projects.cpp
+++ Chapter 6 Programming Projects/Chapter 6 Programming Projects.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 int GetAccidents(string);
-void FindLowest(int north, int south, int east, int west, int central);
+void FindLowest(int North, int South, int East, int West, int Central);
 
 int main()
 {
@@ -25,4 +25,46 @@ int main()
     FindLowest(North, South, East, West, Central); 
 
     return 0;
+}
+
+int GetAccidents(string RegionName) {
+    int Accidents;
+
+    do {
+        cout << RegionName << " region: ";
+        cin >> Accidents;
+
+        if (Accidents < 0) {
+            cout << "Invalid input. Number of accidents must be a whole and positve number.\n";
+        }
+
+    } while (Accidents < 0);
+
+    return Accidents;
+}
+
+void FindLowest(int North, int South, int East, int West, int Central) 
+{
+    string Region = "North";
+    int Lowest = North;
+
+    if (South < Lowest) {
+        Lowest = South;
+        Region = "South";
+    }
+    if (East < Lowest) {
+        Lowest = East; 
+        Region = "East"; 
+    }
+    if (West < Lowest) {
+        Lowest = West;
+        Region = "West";
+    }
+    if (Central < Lowest) {
+        Lowest = Central;
+        Region = "Central";
+    }
+
+    cout << "Region with the fewest reported accidents last year was:\n"
+        << Region;
 }
