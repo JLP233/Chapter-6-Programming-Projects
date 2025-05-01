40ff704acc86c8952ab4a8a2dbd02001d32623d3
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-05-01 10:03:12 -0400

    added code description

--- Chapter 6 Programming Projects/Chapter 6 Programming Projects.cpp
+++ Chapter 6 Programming Projects/Chapter 6 Programming Projects.cpp
@@ -1,30 +1,10 @@
 // Chapter 6 Programming Projects.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-// This program displays a conversion chart for Fahrenheit to Celsius temperatures.
+// This program calculates final scoring for a performance based on 5 judge raw score inputs 0-10.
 
 #include <iostream>
-#include <iomanip>
+
 
 using namespace std;
 
-// F to C Function
-float ConversionToCelsius(int Fahrenheit)
-{
-	return (5.0 / 9.0) * (Fahrenheit - 32);
-}
-
-//Display table function
-void DisplayCTable() {
-	cout << "Fahrenheit to Celsius" << endl;
-	for (int FTemp = 0; FTemp <= 20; FTemp++)
-	{
-		float Celsius = ConversionToCelsius(FTemp);
-		cout << FTemp << " = " << Celsius << endl;
-	}
-}
-
-int main() {
-	DisplayCTable();
-	return 0;
-}
 
